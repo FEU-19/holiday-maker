40765d8160c45385bc9a57648870231c68979afa
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-08-28 14:06:01 +0200

    database setup + config

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load config
+dotenv.config({ path: './config/config.env' });
+
+// Db connection
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
