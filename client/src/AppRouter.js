1fd0917c195b4b5fd14c751f314b79b7c6e1653f
Hampus Olsen <hampus.olsen@outlook.com>
2020-08-31 14:23:25 +0200

    file structure for client and server worked out

---
+++ client/src/AppRouter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Main from "./components/views/Main";
+
+// To add more routes use format as below and add to the routes array
+const routes = [{ path: "/", component: Main }];
+
+function AppRouter() {
+  return (
+    <Switch>
+      {routes.map(({ path, component }) => (
+        <Route exact path={path} component={component} />
+      ))}
+    </Switch>
+  );
+}
+
+export default AppRouter;
