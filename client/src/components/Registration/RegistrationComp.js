88b0459303851e54e7f1c547bc60c33743431171
Martynas Majaras <martynasmajaras98@gmail.com>
2020-09-01 16:28:18 +0200

    Done

--- client/src/components/Registration/RegistrationComp.js
+++ client/src/components/Registration/RegistrationComp.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import { styled } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
+import RenderInputs from './RenderInputs';
+import RenderMsg from './RenderMsg';
+
 import axios from "axios";
 
 const RegistrationComp = () => {
@@ -29,7 +27,6 @@ const RegistrationComp = () => {
     const { name } = e.target;
     setNewUser({ ...newUser, [name]: value.trimStart() });
   }
-
 
   function handleInputReset() {
     setNewUser({
@@ -62,7 +59,6 @@ const RegistrationComp = () => {
       }
     }
   
-
   function handlePostUser() {
     const url = "";
 
@@ -74,7 +70,6 @@ const RegistrationComp = () => {
         setShowMsg(true);
         setWhatMsgToShow(1);
 
-
         setTimeout(() => {
           setShowMsg(false);
         }, 2500);
@@ -83,16 +78,12 @@ const RegistrationComp = () => {
         console.log(err);
         // account finns redan
         setWhatMsgToShow(2);
-
-
         // // Beroende på vilken felmeddelande vi får ska vi ändra till 2:a eller 3:
         // if(){
         //   setWhatMsgToShow(2);
         // } else {
         //   setWhatMsgToShow(5); // denna siffran ska alltid vara högst
         // }
-
-
         setShowMsg(true);  
         setTimeout(() => {
           setShowMsg(false);
@@ -107,20 +98,17 @@ const RegistrationComp = () => {
     console.log("From submit form ", newUser);
     setAnchorEl(e.currentTarget);
 
-    let test;
-    
+    let fieldsEmpty;
     for(let item of Object.values(newUser)){
       console.log(item);
       if(item === ''){
-        test = false;
+        fieldsEmpty = false;
       } else {
-        test = true;
+        fieldsEmpty = true;
       }
     }
 
-
-
-      if(!test){
+      if(!fieldsEmpty){
         setShowMsg(true);
         setWhatMsgToShow(3);
 
@@ -133,191 +121,15 @@ const RegistrationComp = () => {
       }
       
     } 
-
-  
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  let msg;
-  if(whatMsgToShow === 1){
-    msg = <Typography >Account was created.</Typography>;
-  }  else if (whatMsgToShow === 2) {
-    msg = <Typography>Account with this email already exists.</Typography>
-  }
-  else if (whatMsgToShow === 3) {
-    msg = <Typography>Fields can´t be empty.</Typography>
-  }
-  else if (whatMsgToShow === 4) {
-    msg = <Typography>Password doesn´t match.</Typography>
-  }
-  else {
-    msg = <Typography>Something went wrong. try again later.</Typography>
-  }
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
 
   return (
-    <form autoComplete="off" onSubmit={(e) => handleSubmit(e)}>
-            {/* className={classes.root} noValidate */}
-            
-      <TextField
-        id="email"
-        label="Email"
-        type="email"
-        name="email"
-        value={newUser.email}
-        onChange={handleInput}
-        // required
-      />
-            
-      <TextField
-        id="password"
-        label="Password"
-        type="password"
-        name="password"
-        value={newUser.password}
-        onChange={handleInput}
-        // required
-        minLength="1"
-        maxLength="12"
-      />
-
-      <TextField
-        id="password"
-        label="Confirm Password"
-        type="password"
-        name="confirm_password"
-        value={newUser.confirm_password}
-        onChange={handleInput}
-        // required
-        minLength="1"
-        maxLength="12"
-      />
-            
-      <TextField
-        id="first_name"
-        label="First name"
-        type="text"
-        name="first_name"
-        value={newUser.first_name}
-        onChange={handleInput}
-        // required
-        minLength="1"
-        maxLength="30"
-      />
-            
-      <TextField
-        id="surname"
-        label="Surname"
-        type="text"
-        name="surname"
-        value={newUser.surname}
-        onChange={handleInput}
-        // required
-        minLength="1"
-        maxLength="12"
-      />
-            
-      <TextField
-        id="street"
-        label="Street"
-        type="text"
-        name="street"
-        value={newUser.street}
-        onChange={handleInput}
-        // required
-        minLength="1"
-        maxLength="20"
-      />
-            
-      <TextField
-        id="zip_code"
-        label="Zip code"
-        type="text"
-        name="zip_code"
-        value={newUser.zip_code}
-        onChange={handleInput}
-        // required
-        minLength="1"
-        maxLength="10"
-      />
-            
-      <TextField
-        id="city"
-        label="City"
-        type="text"
-        name="city"
-        value={newUser.city}
-        onChange={handleInput}
-        // required
-        minLength="1"
-        maxLength="30"
-      />
-            
-      <TextField
-        id="country"
-        label="Country"
-        type="text"
-        name="country"
-        value={newUser.country}
-        onChange={handleInput}
-        // required
-        minLength="1"
-        maxLength="30"
-      />
-            
-      <TextField
-        id="phone_number"
-        label="Phone number"
-        type="text"
-        name="phone_number"
-        value={newUser.phone_number}
-        onChange={handleInput}
-        // required
-        minLength="1" //10
-        maxLength="15" 
-      />
-            
-      <TextField
-        id="social_security_number"
-        label="social security number"
-        type="text"
-        name="social_security_number"
-        value={newUser.social_security_number}
-        onChange={handleInput}
-        // required
-        minLength="1" //8
-        maxLength="12"
-      />
-      <Button type='submit' variant="contained" color="primary" >
-        Register
-      </Button>
-
-
-
-
-       <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-      >
-        {msg}
-        {/* className={classes.typography} */}
-       </Popover> 
-          
-    </form>
+    <div>
+    <RenderInputs handleSubmit= {handleSubmit} newUser={newUser} handleInput={handleInput}/>
+    <RenderMsg  whatMsgToShow={whatMsgToShow} handleClose={handleClose} anchorEl={anchorEl} />  
+    </div>
   );
 };
 
