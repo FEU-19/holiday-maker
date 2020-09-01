954942628a87c7f73958b2ac90a57bf822e42808
Johanna <nijodotcom@gmail.com>
2020-09-01 09:55:54 +0200

    registation have a route

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Main from "./views/Main";
+import Registration from "./views/Registration";
 
 // To add more routes use format as below and add to the routes array
-const routes = [{ path: "/", component: Main }];
+const routes = [
+  { path: "/", component: Main },
+  { path: "/registration", component: Registration },
+];
 
 function AppRouter() {
   return (
     <Switch>
       {routes.map(({ path, component }) => (
-        <Route exact path={path} component={component} />
+        <Route exact path={path} component={component} key={path} />
       ))}
     </Switch>
   );
