6a9c7bbc83a799ee22847ffd321aa70c9d632611
Daniel Reineborn <d.reineborn@gmail.com>
2020-09-01 12:55:14 +0200

    post route for booking model done, might -probably, needs some work later.

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,29 +1,29 @@
 const Booking = require("../models/Booking");
 const User = require("../models/User");
-const { ObjectId } = require("mongodb");
 
 exports.create = (req, res) => {
   // Anta application/json
   // Lägg in bookingDate i hotell.occupiedDates.
   // ID kommer sparas i cookie.
-  const ID = req.cookies.userId;
+  const Id = req.cookies.userId;
 
-  User.find().where(_id: ObjectId(ID))
-  .then(response => {
-    if (!response) res.status(404).send({error: "hantera error?"})
-    let user = response.data.data;
-    return user;
-  })
-  .then(user => {
-    let data = req.body;
-    data.user = user;
-    const booking = new Booking(data);
-    booking.save();
-
-  })
-
-
-  if (validation === true) res.status(201).json(booking);
+  User.findById(Id)
+    .then((response) => {
+      if (!response)
+        res.status(404).send({ error: "Resource not found, bad ID" });
+      const user = response.data.data;
+      return user;
+    })
+    .then((user) => {
+      const data = req.body;
+      data.user = user;
+      const booking = new Booking(data);
+      booking.save();
+      return res.status(201).json(booking);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 };
 
 exports.read = (_req, res) => {
