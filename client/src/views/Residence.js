1bace3cb4cb0aa468b7e7b47fa328148475424c2
Jesper Bengtsson Ekwall <jeppen@live.com>
2020-09-01 11:49:36 +0200

    Added info boxes

--- client/src/views/Residence.js
+++ client/src/views/Residence.js
@@ -1,9 +1,17 @@
 import React from "react";
 import data from "../components/Residence/residents.json";
+import HotelInformation from "../components/Residence/HotelInformation";
+const hotel = data[0];
 
 const Residence = () => {
   console.log(data);
-  return <div>Residence</div>;
+
+
+  return <div>
+      <h1>{hotel.name}</h1>
+
+      <HotelInformation info={hotel}/>
+    </div>;
 };
 
 export default Residence;
