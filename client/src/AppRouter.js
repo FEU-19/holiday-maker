b49daefb1e485c2be322db34afbdd5bcf0c11a23
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-09-01 11:32:38 +0200

    loginform base

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Login from "./views/Login";
 import Main from "./views/Main";
 
 // To add more routes use format as below and add to the routes array
-const routes = [{ path: "/", component: Main }];
+const routes = [{ path: "/", component: Main },{ path: "/login", component: Login}];
 
 function AppRouter() {
   return (
