c2175b9d90daca1bdb663f555fba9b8ae4f5b2ff
Jonna Svensson <svensson.jonna@hotmail.com>
2020-09-02 10:11:27 +0200

    created main container

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from 'styled-components';
+
+// stor bokstav på variabler i styled components
+const Container = styled.main`
+  display: flex;
+  justify-content: column;
+  height: 100vh;
+  border: 2px solid black; 
+`
 
 const Main = () => {
-  return <div>Index Page</div>;
+  return (
+      <Container>
+
+
+      </Container>
+  )
 };
 
 export default Main;
