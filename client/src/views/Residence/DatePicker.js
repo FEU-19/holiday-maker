fde3453ee9c942910fe3b3ca76ca7ef5c254342c
Daniel Westin <DanielWestin@Daniels-MacBook-Pro.local>
2020-09-02 11:50:00 +0200

    added date-picker

---
+++ client/src/views/Residence/DatePicker.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { TextField } from "@material-ui/core";
+
+export default function DatePicker() {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const yyyy = today.getFullYear();
+
+  const ahead = (parseInt(mm) + 1).toString().padStart(2, "0");
+
+  const now = yyyy + "-" + mm + "-" + dd;
+
+  const oneWeekAhead = yyyy + "-" + ahead + "-" + dd;
+
+  const [bookingDates, setBookingDates] = useState({ start: "", end: "" });
+
+  const handleDate = (e) => {
+    let newBooking = {
+      ...bookingDates,
+      [e.target.name]: e.target.value,
+    };
+
+    setBookingDates(newBooking);
+  };
+
+  return (
+    <form noValidate>
+      <p>Från</p>
+      <TextField
+        name="start"
+        id="date"
+        label="Från"
+        type="date"
+        defaultValue={now}
+        onChange={handleDate}
+      />
+      <p>Till</p>
+      <TextField
+        name="end"
+        id="date"
+        label="Till"
+        type="date"
+        defaultValue={oneWeekAhead}
+        onChange={handleDate}
+      />
+    </form>
+  );
+}
--- client/src/views/Residence/Residence.js
+++ client/src/views/Residence/Residence.js
@@ -3,10 +3,13 @@ import { Grid, Paper, Divider, Button } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { useGrid, useTheme } from "./styles";
 import Carousel from "./Carousel";
+import DatePicker from "./DatePicker";
 
 const Residence = () => {
   const [openModal, setOpenModal] = useState(false);
   const grid = useGrid();
+
+  // const bookingOptions = [All - Inclusive, Helpension, Halvpension];
 
   return (
     <ThemeProvider theme={useTheme}>
@@ -18,22 +21,31 @@ const Residence = () => {
           alignContent="stretch"
           className={grid.rule}
         >
-          <Grid item xs={3} className={grid.section}>
+          <Grid item xs={3} className={grid.section} direction="column">
             <Carousel openModal={openModal} setOpenModal={setOpenModal} />
-            <Button onClick={() => setOpenModal(true)}>Modal</Button>
-            Stfu
+            <img
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.ajorifSUT7IuwtVpeJJrogHaFE%26pid%3DApi&f=1"
+              onClick={() => setOpenModal(true)}
+              style={{ width: "85%", height: "30%" }}
+            />
           </Grid>
           <Divider orientation="vertical" flexItem />
-          <Grid item xs={5} className={grid.section}>
-            Stfu
+          <Grid
+            item
+            xs={5}
+            className={grid.section}
+            direction="column"
+            align="center"
+          >
+            <DatePicker />
           </Grid>
           <Divider orientation="vertical" flexItem />
-          <Grid item xs={2} className={grid.section}>
-            Stfu
+          <Grid item xs={2} className={grid.section} direction="column">
+            PRIS
           </Grid>
           <Divider orientation="vertical" flexItem />
-          <Grid item xs className={grid.section}>
-            Stfu
+          <Grid item xs className={grid.section} direction="column">
+            <Button>Boka</Button>
           </Grid>
         </Grid>
       </Paper>
