9c478ce280428bfbe8b4aea4aba3b1a7000a898f
Jonna Svensson <svensson.jonna@hotmail.com>
2020-09-02 13:22:54 +0200

    created content container

---
+++ client/src/components/Main/ContentContainer/ContentContainer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border: 2px solid black;
+  width: 90vw;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+`
+
+const ContentContainer = () => {
+    return (
+      <Container>
+      </Container>
+    )
+  };
+  
+  export default ContentContainer;
+  
