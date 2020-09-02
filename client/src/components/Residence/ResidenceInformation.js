c7c5b698fa0b96c0d2f701dea061d38d5fbe2bc5
Jesper Bengtsson Ekwall <jeppen@live.com>
2020-09-02 14:05:33 +0200

    Added styles

--- client/src/components/Residence/ResidenceInformation.js
+++ client/src/components/Residence/ResidenceInformation.js
@@ -1,52 +1,70 @@
 import React from "react";
 
+// Style variables
+const boxContainer = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "700px",
+  height: "200px",
+};
+
+const infoContainer = {
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "flex-start",
+  width: "220px",
+  paddingLeft: "25px",
+  paddingTop: "20px",
+  paddingBottom: "20px",
+  marginLeft: "5px",
+  marginRight: "5px",
+  backgroundColor: "#EAEAEA",
+  color: "black",
+  borderRadius: "10px",
+  boxSizing: "border-box"
+};
+
+const infoContent = {
+  display: "flex",
+  flexDirection: "column",
+  paddingTop: "20px",
+  color: "black",
+  boxSizing: "border-box"
+};
+
+const textReset = {
+  margin: "0",
+  padding: "0",
+};
 
 
 
 const ResidenceInformation = ({info}) => {
-
-  // Style variables
-  const boxContainer = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "800px",
-    height: "200px",
-  };
-
-  const infoBoxes = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "200px",
-    height: "200px",
-    margin: "20px",
-    backgroundColor: "lightgrey",
-    color: "black",
-    borderRadius: "10px"
-  };
-
-  return <div className="residence__hotelInformation__boxContainer" style={boxContainer}>
-
-      <div className="residence__hotelInformation__boxContainer__about" style={infoBoxes}>
-        <h3>About</h3>
-        {info.restaurant && <p>Restaurant</p>}
-        {info.pool && <p>Pool</p>}
+  return (
+    <div className="residence__residenceInformation__boxContainer" style={boxContainer}>
+      <div className="residence__residenceInformation__boxContainer__aboutContainer" style={infoContainer}>
+        <h3 style={textReset}>About</h3>
+        <div className="residence__residenceInformation__boxContainer__aboutContent" style={infoContent}>
+          {info.restaurant && <p style={textReset}>Restaurant</p>}
+          {info.pool && <p style={textReset}>Pool</p>}
+        </div>
       </div>
-      <div className="residence__hotelInformation__boxContainer__family" style={infoBoxes}>
-        <h3>For Family</h3>
-        {info.kidsClub && <p>Kids Club</p>}
-
+      <div className="residence__residenceInformation__boxContainer__familyContainer" style={infoContainer}>
+        <h3 style={textReset}>For Family</h3>
+        <div className="residence__residenceInformation__boxContainer__familyContent" style={infoContent}>
+          {info.kidsClub && <p style={textReset}>Kids Club</p>}
         </div>
-      <div className="residence__hotelInformation__boxContainer__nearby" style={infoBoxes}>
-      <h3>Nearby</h3>
-
-        {info.distanceToBeach && <p>Distance to Beach: {info.distanceToBeach} m</p>}
-        {info.distanceToCity && <p>Distance to City: {info.distanceToCity} m</p>}
+      </div>
+      <div className="residence__residenceInformation__boxContainer__nearbyContainer" style={infoContainer}>
+        <h3 style={textReset}>Nearby</h3>
+        <div className="residence__residenceInformation__boxContainer__nearbyContent" style={infoContent}>
+          {info.distanceToBeach && <p style={textReset}>Distance to Beach: {info.distanceToBeach} m</p>}
+          {info.distanceToCity && <p style={textReset}>Distance to City: {info.distanceToCity} m</p>}
         </div>
-
-    </div>;
+      </div>
+    </div>
+  );
 };
 
 export default ResidenceInformation;
--- client/src/views/Residence.js
+++ client/src/views/Residence.js
@@ -3,7 +3,7 @@ import data from "../components/Residence/residents.json";
 import ResidenceInformation from "../components/Residence/ResidenceInformation";
 const hotel = data[0];
 
-// Hotel ID probably will come as props from search team (under discussion).
+// Hotel ID will come as props from search team, but not yet implemented
 const Residence = () => {
   console.log(data);
 
