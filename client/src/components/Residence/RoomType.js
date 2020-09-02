6f32a56e232d73b226f4574692644c81cb184a23
MervatMimmi <mervat.hassan@gmail.com>
2020-09-02 16:30:19 +0200

    test

---
+++ client/src/components/Residence/RoomType.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardHeader, CardMedia, CardContent, Typography } from "@material-ui/core";
+
+const useStyle = makeStyles(() => ({
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%',
+    },
+}))
+
+const RoomType = ({roomType}) => {
+    const classes = useStyle();
+    console.log(roomType);
+
+    
+    return (
+        <Card className = {classes.card}>
+            <CardHeader className = {classes.header}
+                        title = {roomType.type}/>
+            <CardMedia className = {classes.media}
+                        title = {roomType.type}
+                        image = {roomType.images[0]}
+                        />
+            <CardContent>
+                <Typography variant = "body2" color ="textSecondary" component = "p">
+                    {roomType.size}
+                    {roomType.information}
+                </Typography>
+
+            </CardContent>
+
+        </Card>
+    )
+}
+
+export default RoomType;
