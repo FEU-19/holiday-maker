94fcf93c785280da4ce6ba869d390b506af2b0d4
Jonna Svensson <svensson.jonna@hotmail.com>
2020-09-03 15:46:42 +0200

    deleted not needed code

--- client/src/components/Main/SearchContainer/SelectCity.js
+++ client/src/components/Main/SearchContainer/SelectCity.js
@@ -40,11 +40,3 @@ const SelectCity = ({ residentData }) => {
 
 export default SelectCity;
 
-
-
-
-
-{/*                 <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
- */}
