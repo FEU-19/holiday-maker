44d21dcd4abe57865f3b8d05b69bd8ebb1cd1d07
Jesper Bengtsson Ekwall <jeppen@live.com>
2020-09-03 15:21:37 +0200

    Added axios to Residence.js

--- client/src/views/Residence.js
+++ client/src/views/Residence.js
@@ -1,34 +1,45 @@
 import React, {useEffect, useState} from "react";
-import data from "../components/Residence/residents.json";
+//import data from "../components/Residence/residents.json";
 import ResidenceInformation from "../components/Residence/ResidenceInformation";
 import RoomCard from "../components/Residence/RoomCard";
-const hotel = data[0];
-const rooms = data[0].rooms;
+import axios from "axios";
+//const hotel = data[0];
+//const rooms = data[0].rooms;
 
 // Hotel ID will come as props from search team, but not yet implemented
 const Residence = () => {
-  const [residence, updateResidence] = useState({});
+  const [data, updateData] = useState({});
+
 
   useEffect(() => {
     axios.get("http://localhost:8080/api/residents/5f4e2b510ae3bf21d48b0aaf")
       .then(response => {
-        console.log(response);
+        updateData(response.data.data);
+      })
+      .catch(error => {
+        console.error("An error occured while retrieving data from the server");
       });
   },[]);
 
-  console.log(data);
+  //console.log(data);
+  //console.log(residence.rooms);
 
 
-  return <div>
+  if (Object.keys(data).length === 0){
+    return(<p>Loading...</p>);
+  }
+
+  return (
     <div>
-      <h1>{hotel.name}</h1>
-
-      <ResidenceInformation info={hotel}/>
-    </div>;
-    <div>
-    <RoomCard roomsInfo = {rooms}/>
+      <div>
+        <h1>{data.name}</h1>
+        <ResidenceInformation info={data}/>
+      </div>
+      <div>
+        <RoomCard roomsInfo = {data.rooms}/>
+      </div>
     </div>
-  </div>
+  );
 };
 
 export default Residence;
