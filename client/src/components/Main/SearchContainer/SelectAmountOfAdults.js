60da029b4610bc50ade8093c6a137f397fa552e1
Frida <lindstromfrida@yahoo.se>
2020-09-03 14:00:38 +0200

    select of amount of adults input

--- client/src/components/Main/SearchContainer/SelectAmountOfAdults.js
+++ client/src/components/Main/SearchContainer/SelectAmountOfAdults.js
@@ -6,33 +6,34 @@ import MenuItem from '@material-ui/core/MenuItem';
 
 
 const SelectAmountOfAdults = () => {
-  const [adults, setAdults] = React.useState('');
+  const [amountOfAdultsadults, setAmountOfAdults] = React.useState('');
 
-  const handleChange = (event) => {
-    setAdults(event.target.value);
-  };
-    return (
-      <div>
-        <InputLabel id="demo-simple-select-filled-label">Adults</InputLabel>
-        <Select
-          labelId="demo-simple-select-filled-label"
-          id="demo-simple-select-filled"
-          value={adults}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-          </MenuItem>
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={4}>4</MenuItem>
-          <MenuItem value={5}>5</MenuItem>
-          <MenuItem value={6}>6</MenuItem>
-          <MenuItem value={7}>7</MenuItem>
-          <MenuItem value={8}>8</MenuItem>
-        </Select>
-      </div>
-    )
+  const handleChange = (e) => {
+    setAmountOfAdults(e.target.value);
   };
 
-  export default SelectAmountOfAdults;
+  return (
+    <>
+      <InputLabel id="selectAmountOfAdults">Amount of adults</InputLabel>
+      <Select
+        aria-label={"Select amount of adults"}
+        displayEmpty
+        id="selectAmountOfAdults"
+        value={amountOfAdultsadults}
+        onChange={handleChange}
+      >
+        <MenuItem disabled>Amount of adults</MenuItem>
+        <MenuItem value={1}>1</MenuItem>
+        <MenuItem value={2}>2</MenuItem>
+        <MenuItem value={3}>3</MenuItem>
+        <MenuItem value={4}>4</MenuItem>
+        <MenuItem value={5}>5</MenuItem>
+        <MenuItem value={6}>6</MenuItem>
+        <MenuItem value={7}>7</MenuItem>
+        <MenuItem value={8}>8</MenuItem>
+      </Select>
+    </>
+  )
+};
+
+export default SelectAmountOfAdults;
