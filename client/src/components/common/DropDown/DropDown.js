9ebbb65db4ddbb586df634082ad324722256de53
Linus Viberg <linuswiberg96@gmail.com>
2020-09-04 14:43:07 +0200

    Added logic to render right amount of extra beds

--- client/src/components/common/DropDown/DropDown.js
+++ client/src/components/common/DropDown/DropDown.js
@@ -1,95 +1,77 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from 'react';
+import { styled } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import NativeSelect from '@material-ui/core/NativeSelect';
 import Box from '@material-ui/core/Box';
+import axios from 'axios';
 
 
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-    dropdownBox: {
-        width: '200px'
-    }
-}));
 
-const roomsArr = Array.from(Array(20), (_, i) => i + 1);
 
-const extraBedArr = Array.from(Array(Math.floor(Math.random() * 10)), (_, i) => i + 1);
+const MyFormControl = styled(FormControl)({
+        minWidth: 120,
+        marginBottom: 20
+});
 
-console.log('extraBedArr', extraBedArr)
+const MyBox = styled(Box)({
+    width: '200px',
+    display: 'flex',
+    flexDirection: 'column'
+})
+
 const DropDown = () => {
-    const classes = useStyles();
-
-    let arr = [];
-    
-    for (let i = 1; i <= arr.length; i++) {
-        arr.push(i);
-    }
-
     const [state, setState] = React.useState(0);
+    const [roomsArr, setRoomsArr] = React.useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/api/residents/5f5230fafd504a310c8184dd/')
+            .then((res) => {
+                setRoomsArr(res.data.data.rooms);
+            })
+      }, []);
 
     const handleChange = (event) => {
         setState(event.target.value)
     };
 
-    const handleClick = () => {
-        console.log(state);
-    }
-
-
     return (
-        <Box 
-            className={classes.dropdownBox}
-            display="flex"
-            flexDirection="column"
-        >
-            <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="native-simple">
-                    № rooms
-                </InputLabel>
-                <NativeSelect
-                    value={state}
-                    onChange={handleChange}
-                >
-                    <option aria-label="" value="" />
-                    { roomsArr.map(v => {
-                        return <option value={v} key={v}>{v}</option>
-                    })}
-                </NativeSelect>
-            </FormControl>
-            {
-            extraBedArr.length < 0
-            ?   <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="native-simple">
-                    Extra beds
-                    </InputLabel>
-                    <NativeSelect
-                        value={state}
-                        onChange={handleChange}
-                    >
-                        <option aria-label="" value="" />
-                        { extraBedArr.map(v => {
-                            return <option value={v} key={v}>{v}</option>
-                        })}
-                    </NativeSelect>
-                </FormControl>
-            :   <FormControl className={classes.formControl} >
-                    <InputLabel htmlFor="name-native-disabled" disabled>Extra beds</InputLabel>
-                    <NativeSelect disabled>
-                    </NativeSelect>
-                </FormControl>
-        }
-        </Box>
+        <MyBox>
+            { roomsArr.map(v => {
+                return (
+                    <>
+                        <p>{v.roomNumber}</p>
+                        {   v.extraBed > 0
+                            ?   <MyFormControl>
+                                    <InputLabel htmlFor="native-simple">
+                                    Extra beds
+                                    </InputLabel>
+                                    <NativeSelect
+                                        value={state}
+                                        onChange={handleChange}
+                                    >
+                                        <option aria-label="" value="" />
+                                        {   Array.from(Array(v.extraBed), (_, i) => i + 1).map( val => {
+                                            return <option value={val} key={val}>{val}</option>
+                                        })
+
+                                        }
+
+                                    </NativeSelect>
+                                </MyFormControl>
+                            :   <MyFormControl>
+                                    <InputLabel htmlFor="name-native-disabled" disabled>Extra beds</InputLabel>
+                                    <NativeSelect disabled>
+                                    </NativeSelect>
+                                </MyFormControl>
+                        }
+                    </>
+                ) 
+            })
+            }
+        </MyBox>
         
     )    
 }
  
 export default DropDown;
-
