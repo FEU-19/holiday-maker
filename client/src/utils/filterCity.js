c4295ff59ed3ce8f66216b44e7911830880af640
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-09-04 09:42:54 +0200

    change arg name

--- client/src/utils/filterCity.js
+++ client/src/utils/filterCity.js
@@ -2,8 +2,8 @@
 // 
 // Filter out hotels which does not match userInput
 
-export default function filterCity(city, userInput) {
-  let x = city.filter((hotel) => {
+export default function filterCity(data, userInput) {
+  let x = data.filter((hotel) => {
     return hotel.city.toLowerCase() === userInput.toLowerCase();
   })
   return x;
