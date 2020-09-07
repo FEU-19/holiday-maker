06b05a74832b54c4b3b304f59d62ebd788488aef
Cecilia <cecilia.svensson.is@gmail.com>
2020-09-07 13:31:58 +0200

    testing test

--- client/src/components/Login/Login.test.js
+++ client/src/components/Login/Login.test.js
@@ -2,12 +2,34 @@ import "@testing-library/jest-dom/extend-expect";
 import ReactDOM from 'react-dom'
 import LoginView from "../../views/Login";
 import React from "react";
+import Login from "./Login"
 
 it ('renders without crashing', () => {
     const Container = document.createElement('Container');
     ReactDOM.render(<LoginView />, Container)
 });
 
-it  ('store email when click Login-button', () => {
-    
-});
+it ()
+
+// test('Create a valid Service', async(done) => {
+//     const service = {
+//         email: "bob@bob.com",
+//         password: "123"
+//     };
+//     try {
+//         const count = await Service.count();
+//         await request(app).post('/api/services').send(service)
+//         const newCount = await Service.count()
+//         expect(newCount).toBe(count + 1);
+//         done()
+//     } catch (err) {
+//         // write test for failure here
+//         console.log(`Error ${err}`)
+//         done();
+//     }
+// });
+
+// it("email must be an email when submiting", () => {
+//     const onMock = jest.fn();
+//     const wrapper = mount(<NoAuthPasswordChange)
+// })
