cbbf33c7e1da6eed8077e5ac0af6f9a214ed9d79
Ted Larsson <larssonted86@gmail.com>
2020-09-07 15:25:45 +0200

    added price change

--- client/src/views/Residence.js
+++ client/src/views/Residence.js
@@ -1,45 +1,48 @@
 import React, { useEffect, useState } from "react";
-import {box} from "@material-ui/core/box";
+// import {box} from "@material-ui/core/box";
 import ResidenceInformation from "../components/Residence/ResidenceInformation";
 import GeneralInformation from "../components/Residence/GeneralInformation";
-import HotelCarousel from '../components/Residence/HotelCarousel';
-import RoomCardMapper from '../components/Residence/RoomCardMapper';
+import HotelCarousel from "../components/Residence/HotelCarousel";
+import RoomCardMapper from "../components/Residence/RoomCardMapper";
 import axios from "axios";
-
 
 // Hotel ID will come as props from search team, but not yet implemented
 const Residence = () => {
-  const [data, updateData] = useState({});
+ const [data, updateData] = useState({});
 
-  useEffect(() => {
-    axios.get("http://localhost:8080/api/residents/5f5230fbfd504a310c818546")
-      .then(response => {
-        updateData(response.data.data);
-      })
-      .catch(error => {
-        console.error("An error occured while retrieving data from the server", error);
-      });
-  }, []);
+ useEffect(() => {
+  axios
+   .get("http://localhost:8080/api/residents/5f5230fbfd504a310c818546")
+   .then((response) => {
+    updateData(response.data.data);
+   })
+   .catch((error) => {
+    console.error(
+     "An error occured while retrieving data from the server",
+     error
+    );
+   });
+ }, []);
 
-  if (Object.keys(data).length === 0) {
-    return (<p>Loading...</p>);
-  }
+ if (Object.keys(data).length === 0) {
+  return <p>Loading...</p>;
+ }
 
-  return (
-    <div>
-      <div>
-        <h1>{data.name}</h1>
-        <HotelCarousel/>
-        <ResidenceInformation info={data} />
-      </div>
-      <div>
-        <RoomCardMapper allRooms={data.rooms} />
-      </div>
-      <div>
-        <GeneralInformation generalInfo={data} />
-      </div>
-    </div>
-  );
+ return (
+  <div>
+   <div>
+    <h1>{data.name}</h1>
+    <HotelCarousel />
+    <ResidenceInformation info={data} />
+   </div>
+   <div>
+    <RoomCardMapper allRooms={data.rooms} />
+   </div>
+   <div>
+    <GeneralInformation generalInfo={data} />
+   </div>
+  </div>
+ );
 };
 
 export default Residence;
