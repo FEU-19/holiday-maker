1641718fd14b89ef71267eab32b3a16e5d5e2ecd
Johanna <nijodotcom@gmail.com>
2020-09-07 08:59:10 +0200

    nothing works

--- client/src/components/Registration/Registration.test.js
+++ client/src/components/Registration/Registration.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import ReactDOM from 'react-dom'
 
 import Registration from "../../views/Registration";
@@ -10,40 +10,44 @@ import RegisterButton from './RegisterButton';
 
 it ('renders without crashing', () => {
     const Container = document.createElement('Container');
+    console.log(Container);
     ReactDOM.render(<Registration />, Container)
 });
 
 
 describe('Form', () => {
+    const newUser ={
+        first_name: "",
+        email: "",
+        surname: "",
+        street: "",
+        zip_code: "",
+        city: "",
+        country: "",
+        phone_number: "",
+        social_security_number: "",
+        password: "",
+        confirm_password: "",
+      }
+   
     it('Finds the first input', () => {
-        const wrapper = shallow(<RenderInputs />)
-        const form = wrapper.find('form');
-        let val = 'first_name'
-        let nameInput = form.find('input' .value(val)).first();
+        const wrapper = shallow(<RenderInputs newUser={newUser}/>);
+        let nameInput = wrapper.find('input').first();
+        console.log(wrapper.length );
+        console.log(nameInput);
 
-        expect(nameInput);
+        
+        
+        // nameInput.simulate('change', {
+        //     target: {value: 'Bob'}
+        // })
+      
+        
+
+        // expect(wrapper.find('form').exists())
+        // .toBe(true);
+        
+        // expect(nameInput.state().value).toEqual('');
     });
 });
 
-// // import BasicInput from './basic-input';
-
-
-// describe('basic input component', () => {
-//     it('should renders without crashing', () => {
-//         const div = document.createElement('div');
-//         ReactDOM.render(<RenderInputs />, div);
-//         ReactDOM.unmountComponentAtNode(div);
-//     });
-
-//     it('should render a placeholder', () => {
-//         const placeholder_text = 'type anything here';
-//         const wrapper = shallow(<RenderInputs placeholder={placeholder_text} />);
-//         expect(wrapper.prop('placeholder')).toEqual(placeholder_text);
-//     });
-
-//     it('should render a correct type', () => {
-//         const type = 'password';
-//         const wrapper = shallow(<RenderInputs type={type} />);
-//         expect(wrapper.prop('type')).toEqual(type);
-//     });
-// });
