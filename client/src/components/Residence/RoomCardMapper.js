5ce1724570e95529f3a4724e86c58c54f5bdf871
Hampus Olsen <hampus.olsen@outlook.com>
2020-09-08 15:43:22 +0200

    nb

--- client/src/components/Residence/RoomCardMapper.js
+++ client/src/components/Residence/RoomCardMapper.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import RoomCard from "./RoomCard";
 
-const RoomCardMapper = ({allRooms}) => {
-  console.log(allRooms);
-  return(
+const RoomCardMapper = ({ allRooms }) => {
+  const [chosenRooms, setChosenRooms] = useState([]);
+
+  const chooseRoom = (room) => {
+    setChosenRooms((rooms) => {
+      if (!!rooms.find((_room) => room._id === _room._id)) {
+        return rooms.filter((_room) => _room._id !== room._id);
+      }
+
+      return [...rooms, room];
+    });
+  };
+
+  console.log(chosenRooms);
+
+  return (
     <div>
-      {allRooms.map(room => {
-        return(
-          <RoomCard roomInfo={room} />
-        );
+      {allRooms.map((room) => {
+        return <RoomCard roomInfo={room} chooseRoom={chooseRoom} />;
       })}
     </div>
   );
