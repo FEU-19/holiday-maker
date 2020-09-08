6d5d075e2be32d86eb66c53da24ba9d0243faa1e
Jesper Bengtsson Ekwall <jeppen@live.com>
2020-09-08 14:21:04 +0200

    Added temp route

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Main from "./views/Main";
+import Residence from "./views/Residence";
 
 // To add more routes use format as below and add to the routes array
 const routes = [
   { path: "/", component: Main },
+  { path: "/residence", component: Residence }
 ];
 
 function AppRouter() {
