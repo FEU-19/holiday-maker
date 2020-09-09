e8842421e3471a659cbbc0bbc659226c7cadde7d
Rubi Kuo <rubitiara@gmail.com>
2020-09-09 09:25:12 +0200

    add modal style

--- client/src/components/Checkout/PaymentStyles.js
+++ client/src/components/Checkout/PaymentStyles.js
@@ -122,3 +122,48 @@ export {
   EXPCVCInput,
   PayBtn,
 };
+
+// Modal style
+export const ModalStyle = makeStyles(() => ({
+  constainer: {
+    position: "relative",
+  },
+  content: {
+    width: "30rem",
+    height: "20rem",
+    backgroundColor: "white",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    borderRadius: "5px",
+  },
+  closeBtn: {
+    width: "2rem",
+    height: "2rem",
+    position: "absolute",
+    right: "0.1rem",
+    top: "0.1rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent",
+    border: "none",
+  },
+  closeIcon: {
+    color: "grey",
+  },
+  errorIcon: {
+    color: "red",
+    fontSize: "3rem",
+  },
+  checkIcon: {
+    color: "green",
+    fontSize: "3rem",
+  },
+}));
