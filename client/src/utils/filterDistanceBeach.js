54b0eb8b58e664eacbccc6e1fc4b1be6ac4578ba
Frida <lindstromfrida@yahoo.se>
2020-09-10 15:01:59 +0200

    fix console log

--- client/src/utils/filterDistanceBeach.js
+++ client/src/utils/filterDistanceBeach.js
@@ -1,10 +1,7 @@
 export default function filterDistanceBeach (data, userInput = 0) {
     if (!userInput) return data;
 
-   console.log(userInput);
     return data.filter((hotel) => {
-      console.log(hotel.distanceToBeach);
       return hotel.distanceToBeach <= userInput;
-
     });
 };
