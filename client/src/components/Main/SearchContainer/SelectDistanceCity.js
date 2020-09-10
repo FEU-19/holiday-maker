555d0894fe9664e9bae16bf6460fae42a16bc1b0
Frida <lindstromfrida@yahoo.se>
2020-09-10 11:16:49 +0200

    fix

--- client/src/components/Main/SearchContainer/SelectDistanceCity.js
+++ client/src/components/Main/SearchContainer/SelectDistanceCity.js
@@ -23,7 +23,7 @@ const SelectDistanceCity = ({distanceCity, setDistanceCity}) => {
 
   return (
     <>
-      <InputLabel id="selectDistanceCity">Distance to center</InputLabel>
+      <InputLabel id="selectDistanceCity">Distance to the center</InputLabel>
       <Select
         aria-label="Select Distance To City"
         displayEmpty
