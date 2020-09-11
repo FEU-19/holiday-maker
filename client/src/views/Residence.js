82f899ffd249b3c08603134a702ed395c13d7804
Jesper Bengtsson Ekwall <jeppen@live.com>
2020-09-11 13:37:28 +0200

    Spinner

--- client/src/views/Residence.js
+++ client/src/views/Residence.js
@@ -4,6 +4,7 @@ import ResidenceInformation from "../components/Residence/ResidenceInformation";
 import GeneralInformation from "../components/Residence/GeneralInformation";
 import HotelCarousel from "../components/Residence/HotelCarousel";
 import RoomCardMapper from "../components/Residence/RoomCardMapper";
+import ResidenceSpinner from "../components/Residence/ResidenceSpinner";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -21,32 +22,59 @@ const useStyle = makeStyles(() => ({
   info: {
     padding: "2vw",
   },
+  spinner: {
+    width: "100%",
+    height: "70vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  }
 }));
 
 let wait = null;
 // Hotel ID will come as props from search team, but not yet implemented
 const Residence = () => {
+  const [shouldSpin, setShouldSpin] = useState(false);
   const classes = useStyle();
   const [data, updateData] = useState(null);
   const { hotelId } = useParams();
 
+
   useEffect(() => {
+    wait = setTimeout(() => {
+      setShouldSpin(true);
+    },500);
+
     axios
       .get(`http://localhost:8080/api/residences/${hotelId}`)
       .then((response) => {
         updateData(response.data.data);
       })
-      .catch((error) => {
-        console.error(
-          "An error occured while retrieving data from the server",
-          error
-        );
-      });
-  }, [hotelId]);
+      .then(() => {
+       clearTimeout(wait);
+       wait = null;
+       setShouldSpin(false);
+     })
+     .catch((error) => {
+      console.error(
+       "An error occured while retrieving data from the server",
+       error
+      );
+     });
+    }, []);
 
-  if (!data) {
-    return <p>Loading...</p>;
-  }
+
+    if (!data && !shouldSpin){
+     return <div />;
+   }
+
+   if (shouldSpin){
+       return (
+         <div className={classes.spinner}>
+           <ResidenceSpinner />
+         </div>);
+   }
 
   return (
     <div className={classes.article}>
