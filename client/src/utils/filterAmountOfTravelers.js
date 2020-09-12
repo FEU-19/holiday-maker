422d2e1deab6546dafe9fbd42f6c1921d15e23cd
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-09-12 18:52:22 +0200

    now also filter on the age of children

--- client/src/utils/filterAmountOfTravelers.js
+++ client/src/utils/filterAmountOfTravelers.js
@@ -1,9 +1,21 @@
 export default function filterAmountOfTravelers(c, adults, children) {
   let data = JSON.parse(JSON.stringify(c));
+  let amountOfTravelers = null;
 
-  for (let i = 0; i < data.length; i++) {
-    data[i].rooms = data[i].rooms.filter(room => room.beds >= adults);
+  // Removes children under the age of 3
+  for (let i = 0; i < children.length; i++) {
+    if (children[i] < 3) {
+      children.splice(i, 1);
+    }
   }
 
+  amountOfTravelers = adults + children.length;
+
+  // removes rooms which does not contain enough beds
+  for (let i = 0; i < data.length; i++) {
+    data[i].rooms = data[i].rooms.filter(room => room.beds >= amountOfTravelers);
+  }
+
+  // removes hotels 
   return data.filter(hotel => hotel.rooms.length !== 0);
 };
