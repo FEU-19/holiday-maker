f2d677e57492f1b51603d9ad08f608caf7474fe1
Johanna <nijodotcom@gmail.com>
2020-09-14 15:39:40 +0200

    setup the base to the module

--- client/src/components/modals/ChangeBooking/ChangeBookingModal.js
+++ client/src/components/modals/ChangeBooking/ChangeBookingModal.js
@@ -1,18 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import DialogActions from '@material-ui/core/DialogActions';
+import Button from "@material-ui/core/Button";
+// import TextField from "@material-ui/core/TextField";
+import DialogActions from "@material-ui/core/DialogActions";
+import RadioGroup from "@material-ui/core/RadioGroup";
 
 import SelectAmountOfAdults from "../../Main/SearchContainer/SelectAmountOfAdults";
 import SelectAmountOfChildren from "../../Main/SearchContainer/SelectAmountOfChildren";
+import DatePicker from "../../Main/SearchContainer/DatePicker";
 
 import ChildrenAgeSelects from "../../Main/SearchContainer/ChildrenAgeSelects";
+import RenderFoodOption from "./RenderFoodOption";
+
 
 export default function ChangeBookingModal() {
   const [open, setOpen] = useState(true);
+  const [hotelId, setHotelId] = useState('5f5b7e5b36ac0355705b8087');
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/residences/${hotelId}`)
+      .then((resp) => {
+        console.log(resp);
+        // updateData(response.data.data);
+      })
+      .catch((error) => {
+        console.error(
+          "An error occured while retrieving data from the server",
+          error
+        );
+      });
+  }, [hotelId]);
+  // VÄLJA MAT OPTION
+  // hitta room på room[]._id
+  // rendera ut vad rummet erbjuder
+  // skicka in datan till RenderFoodOption
+
+
+  // Få PUT att funka innan vi redirectar¨
+  // Sedan Redirect to={{ pathname: "/checkout", state: { rooms: chosenRooms } }}
+  // const [chosenRooms, setChosenRooms] = useState([]);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -29,6 +59,7 @@ export default function ChangeBookingModal() {
     >
       <DialogTitle id="form-dialog-title">Hotel name</DialogTitle>
       <DialogContent>
+        <DatePicker />
         <SelectAmountOfAdults />
         <SelectAmountOfChildren />
         <ChildrenAgeSelects />
@@ -36,17 +67,27 @@ export default function ChangeBookingModal() {
         {/* Extra bed */}
         {/* flight */}
         {/* Need price */}
+
+        <RadioGroup
+          aria-label="price"
+          name="price"
+          // value={selected}
+          // onChange={handleChange}
+        > 
+        <RenderFoodOption />
+        </RadioGroup>
+
         <Button onClick={handleClose} color="primary">
-            Book flight
-          </Button>
+          Book flight
+        </Button>
       </DialogContent>
       <DialogActions>
-      <Button onClick={handleClose} color="primary">
-            Cancel changes
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Save changes
-          </Button>
+        <Button onClick={handleClose} color="primary">
+          Cancel changes
+        </Button>
+        <Button onClick={handleClose} color="primary">
+          Save changes
+        </Button>
       </DialogActions>
     </Dialog>
   );
---
+++ client/src/components/modals/ChangeBooking/RenderFoodOption.js
@@ -0,0 +1,76 @@
+import React from "react";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { Radio } from "@material-ui/core";
+
+export default function RenderFoodOptions() {
+  return (
+    <p>Hej</p>
+    //   <FormControlLabel
+    //   style={{ borderTop: "1px solid #ccd9dd" }}
+    //   value={roomInfo.allInclusive.toString()}
+    //   control={<Radio color="default" />}
+    //   disabled={roomInfo.allInclusive ? "" : "disabled"}
+    //   label={
+    //     roomInfo.allInclusive ? (
+    //       <p style={{ paddingRight: "10vw" }}>
+    //         All Inclusive: {roomInfo.allInclusive + ":-"}
+    //       </p>
+    //     ) : (
+    //       <p style={{ paddingRight: "10vw" }}>
+    //         All Inclusive: {"N/A"}
+    //       </p>
+    //     )
+    //   }
+    //   labelPlacement="start"
+    // />
+    // <FormControlLabel
+    //   style={{ borderTop: "1px solid #ccd9dd" }}
+    //   value={roomInfo.fullBoard.toString()}
+    //   control={<Radio color="default" />}
+    //   disabled={roomInfo.fullBoard ? "" : "disabled"}
+    //   label={
+    //     roomInfo.fullBoard ? (
+    //       <p style={{ paddingRight: "10vw" }}>
+    //         Half Board: {roomInfo.fullBoard + ":-"}
+    //       </p>
+    //     ) : (
+    //       <p style={{ paddingRight: "10vw" }}>
+    //         Full Board: {"N/A"}
+    //       </p>
+    //     )
+    //   }
+    //   labelPlacement="start"
+    // />
+    // <FormControlLabel
+    //   style={{ borderTop: "1px solid #ccd9dd" }}
+    //   value={roomInfo.halfBoard.toString()}
+    //   control={<Radio color="default" />}
+    //   disabled={roomInfo.halfBoard ? "" : "disabled"}
+    //   label={
+    //     roomInfo.halfBoard ? (
+    //       <p style={{ paddingRight: "10vw" }}>
+    //         Half Board: {roomInfo.halfBoard + ":-"}
+    //       </p>
+    //     ) : (
+    //       <p style={{ paddingRight: "10vw" }}>
+    //         Half Board: {"N/A"}
+    //       </p>
+    //     )
+    //   }
+    //   labelPlacement="start"
+    // />
+    // <FormControlLabel
+    //   style={{ borderTop: "1px solid #ccd9dd" }}
+    //   value={roomInfo.selfCatering.toString()}
+    //   control={<Radio color="default" />}
+    //   label={
+    //     roomInfo.selfCatering || (
+    //       <p style={{ paddingRight: "10vw" }}>
+    //         Self Catering: {roomInfo.selfCatering + ":-"}
+    //       </p>
+    //     )
+    //   }
+    //   labelPlacement="start"
+    // />
+  );
+}
