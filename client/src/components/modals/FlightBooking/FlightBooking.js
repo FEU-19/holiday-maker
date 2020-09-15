6322f86d213aa6b9d6e4c952b5d11a38c0857416
Martynas Majaras <martynasmajaras98@gmail.com>
2020-09-15 16:26:09 +0200

    Almost Done

--- client/src/components/modals/FlightBooking/FlightBooking.js
+++ client/src/components/modals/FlightBooking/FlightBooking.js
@@ -13,25 +13,25 @@ import { makeStyles } from "@material-ui/core/styles";
     // Endpoints: url:/api/my_booking/:ordernr,
     // Request method: PUT
     Response: {
-        dateTo: '[from (19 sep), to (22 oct)]',
-        dateHome:' [from (USA), to (Sweden)]',
-        flightNumber: 2,
+        dateTo: 'from 19 sep, to 20 sep',
+        dateHome:' from 27 oct, to 27 oct',
+        flightNumber: '2',
         flightComany: 'AirFrance',
-        destination: '[from (Stockholm), to (Los Angeles)]',
-        flightTimeTo: '[from (09:00), to (17:00)]',
-        flightTimeHome: '[19:00 (int), to (06:00)]',
+        destination: 'from Stockholm, to Los Angeles',
+        flightTimeTo: 'from 09:00, to 17:00',
+        flightTimeHome: '19:00, to 06:00',
         seats: '1'
     }
 },
 {
     Response: {
-        dateTo: '[from (19 sep), to (22 oct)]',
-        dateHome: '[from (USA), to (Sweden)]',
-        flightNumber: 2,
+        dateTo: 'from 10 dec, to 11 dec',
+        dateHome: 'from 4 jan, to 5 jan',
+        flightNumber: '2',
         flightComany: 'AirFrance',
-        destination: '[from (Stockholm), to (Los Angeles)]',
-        flightTimeTo: '[from (09:00), to (17:00)]',
-        flightTimeHome: '[19:00 (int), to (06:00)]',
+        destination: 'from Los Angeles, to Stockholm',
+        flightTimeTo: 'from 13:00, to 21:00',
+        flightTimeHome: 'from 11:00, to 15:00',
         seats: '1'
     }
 }]
@@ -41,7 +41,10 @@ import { makeStyles } from "@material-ui/core/styles";
     flightlist: {
       width: '100%',
       display: 'flex',
-      flexDirection: 'column',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      fontSize: '15px',
+      margin: '5px',
     },
   }));
 
@@ -65,18 +68,18 @@ console.log(flights.Response)
         <List>
           {flights.map(flight => (
             <ListItem className={classes.flightlist}>  
-              <p>{flight.Response.dateTo}</p>
-              <p>{flight.Response.dateHome}</p>
-              <p>{flight.Response.flightComany}</p>
-              <p>{flight.Response.destination}</p>
-              <p>{flight.Response.flightTimeTo}</p>
-              <p>{flight.Response.flightTimeHome}</p>
-              <p>{flight.Response.seats}</p>
-              <Button>Book Flight</Button>
-              <Button>Cancel</Button>
+              <p className={classes.flightlist}>{flight.Response.dateTo}</p>
+              <p className={classes.flightlist}>{flight.Response.dateHome}</p>
+              <p className={classes.flightlist}>{flight.Response.flightComany}</p>
+              <p className={classes.flightlist}>{flight.Response.destination}</p>
+              <p className={classes.flightlist}>{flight.Response.flightTimeTo}</p>
+              <p className={classes.flightlist}>{flight.Response.flightTimeHome}</p>
+              <p className={classes.flightlist}>{flight.Response.seats}</p>
+                <Button>Book Flight</Button>
             </ListItem>
           ))}
         </List>
+        <Button>Cancel</Button>
       </Dialog>
       </div>
     );
