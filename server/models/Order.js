17d0f8abfbb492a4b3309bef1e7bbe57bb5b4540
Daniel Reineborn <d.reineborn@gmail.com>
2020-09-15 12:13:56 +0200

    fixed validation if room get booked while customer is trying to book same room.

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Hotel = require("./Hotel");
 
 const orderSchema = mongoose.Schema(
   {
@@ -25,6 +26,29 @@ const orderSchema = mongoose.Schema(
     timestamp: true,
   }
 );
+orderSchema.pre("save", async function (next) {
+  const hotel = await Hotel.findOne({ _id: this.hotel });
+  const theseRooms = this.rooms.map((x) => x.roomNumber);
+  // rooms that this order will update.
+  let conflict = hotel.rooms.filter((room) => {
+    return theseRooms.includes(room.roomNumber);
+  });
+  conflict = conflict.reduce((acc, cur) => {
+    return [...acc, ...cur.occupiedDates];
+  }, []);
+  conflict = conflict.reduce((acc, cur) => {
+    if (acc) return acc;
+    const ordS = new Date(this.bookingDates.start);
+    const ordE = new Date(this.bookingDates.end);
+
+    return ordS < new Date(cur.end) && ordE > new Date(cur.start);
+  }, false);
+  // conflict is true or false depending if dates are in conflict.
+  if (conflict) {
+    return next(new Error("Room is already booked."));
+  }
+  return next();
+});
 
 const Order = mongoose.model("order", orderSchema);
 
