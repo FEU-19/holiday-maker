50cc741725a41fd2984532bac86b38fbc9291b6f
Anna Eklöv <annaeklov90@gmail.com>
2020-09-16 10:31:54 +0200

    fixed footer to bottom and footer and header to all pages

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import styled from "styled-components";
 import Main from "./views/Main";
 import Checkout from "./views/Checkout";
 import DropDown from "./components/common/DropDown/DropDown";
 import Residence from "./views/Residence";
+import HeaderComp from "./components/common/Header/Header";
+import Footer from "./components/common/Footer/Footer";
 
 // To add more routes use format as below and add to the routes array
 const routes = [
@@ -13,13 +16,27 @@ const routes = [
   { path: "/dropdown/:id", component: DropDown },
 ];
 
+const DivRoot = styled.div`
+  min-height: 100vh;
+  position: relative;
+`;
+
 function AppRouter() {
   return (
-    <Switch>
-      {routes.map(({ path, component }) => (
-        <Route exact path={path} key={path + component} component={component} />
-      ))}
-    </Switch>
+    <DivRoot>
+      <HeaderComp />
+      <Switch>
+        {routes.map(({ path, component }) => (
+          <Route
+            exact
+            path={path}
+            key={path + component}
+            component={component}
+          />
+        ))}
+      </Switch>
+      <Footer />
+    </DivRoot>
   );
 }
 
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -4,14 +4,13 @@ import styled from "styled-components";
 import SearchContainer from "../components/Main/SearchContainer/SearchContainer";
 import ContentContainer from "../components/Main/ContentContainer/ContentContainer";
 import SortContainer from "../components/Main/SortContainer/SortContainer";
-import HeaderComp from "../components/common/Header/Header";
-import Footer from "../components/common/Footer/Footer";
 
 
 const Container = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding-bottom: 50px;
 `;
 
 const Main = () => {
@@ -26,13 +25,11 @@ const Main = () => {
 
   return (
     <>
-      <HeaderComp />
       <Container>
         <SearchContainer setFilteredDataCB={setFilteredDataCB} setSearching={setSearching} setQueryParams={setQueryParams} />
         <SortContainer filteredData={filteredData} setSortOn={setSortOn} sortOn={sortOn}  />
         <ContentContainer filteredData={filteredData} sortOn={sortOn} searching={searching} queryParams={queryParams} />
       </Container>
-      <Footer/>
     </>
   );
 };
