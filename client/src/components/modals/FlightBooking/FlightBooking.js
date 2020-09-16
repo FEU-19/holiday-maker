fb1794d2955f9254c4651d1cbd13c7156c5e0a0d
Martynas Majaras <martynasmajaras98@gmail.com>
2020-09-16 11:24:09 +0200

    Done

--- client/src/components/modals/FlightBooking/FlightBooking.js
+++ client/src/components/modals/FlightBooking/FlightBooking.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import Modal from '@material-ui/core/Modal';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
  const flights = [
-     {
-    // Format: JSON,
-    // Standard: REST,
-    // Endpoints: url:/api/my_booking/:ordernr,
-    // Request method: PUT
+{
     Response: {
         dateTo: 'from 19 sep, to 20 sep',
         dateHome:' from 27 oct, to 27 oct',
@@ -33,54 +33,114 @@ import { makeStyles } from "@material-ui/core/styles";
         flightTimeTo: 'from 13:00, to 21:00',
         flightTimeHome: 'from 11:00, to 15:00',
         seats: '1'
-    }
+    },
+      
+},
+{
+    Response: {
+        dateTo: 'from 10 dec, to 11 dec',
+        dateHome: 'from 4 jan, to 5 jan',
+        flightNumber: '2',
+        flightComany: 'AirFrance',
+        destination: 'from Los Angeles, to Stockholm',
+        flightTimeTo: 'from 13:00, to 21:00',
+        flightTimeHome: 'from 11:00, to 15:00',
+        seats: '1'
+  },
+  
+},
+{ 
+    Response: {
+        dateTo: 'from 11 dec, to 12 dec',
+        dateHome: 'from 5 jan, to 6 jan',
+        flightNumber: '2',
+        flightComany: 'AirFrance',
+        destination: 'from Los Angeles, to Stockholm',
+        flightTimeTo: 'from 13:00, to 21:00',
+        flightTimeHome: 'from 11:00, to 15:00',
+        seats: '1'
+  },
+},
+{
+    Response: {
+        dateTo: 'from 10 dec, to 11 dec',
+        dateHome: 'from 4 jan, to 5 jan',
+        flightNumber: '2',
+        flightComany: 'AirFrance',
+        destination: 'from Los Angeles, to Stockholm',
+        flightTimeTo: 'from 13:00, to 21:00',
+        flightTimeHome: 'from 11:00, to 15:00',
+        seats: '1'
+  },
 }]
 
-
  const useStyles = makeStyles((theme) => ({
-    flightlist: {
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      fontSize: '15px',
-      margin: '5px',
+  paper: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80%',
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+    table: {
+      minWidth: 650,
     },
   }));
 
-
 const FlightBooking = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = React.useState(false);
 
-    const classes = useStyles();
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-function openModal() {
-    setOpen(true)
-    console.log(flights)
-}
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-console.log(flights.Response)
+  const classes = useStyles();
+
     return (
         <div>
-           <Button onClick={openModal}>Open Modal</Button> 
-      <Dialog  aria-labelledby="simple-dialog-title" open={open}>
-        <DialogTitle id="simple-dialog-title">Available Flights</DialogTitle>
-        <List>
-          {flights.map(flight => (
-            <ListItem className={classes.flightlist}>  
-              <p className={classes.flightlist}>{flight.Response.dateTo}</p>
-              <p className={classes.flightlist}>{flight.Response.dateHome}</p>
-              <p className={classes.flightlist}>{flight.Response.flightComany}</p>
-              <p className={classes.flightlist}>{flight.Response.destination}</p>
-              <p className={classes.flightlist}>{flight.Response.flightTimeTo}</p>
-              <p className={classes.flightlist}>{flight.Response.flightTimeHome}</p>
-              <p className={classes.flightlist}>{flight.Response.seats}</p>
-                <Button>Book Flight</Button>
-            </ListItem>
-          ))}
-        </List>
-        <Button>Cancel</Button>
-      </Dialog>
+          <Button onClick={handleOpen}>Open Modal</Button> 
+          <Modal open={open} onClose={handleClose} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
+            <TableContainer component={Paper}>
+            <Table className={classes.paper} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="right">Date</TableCell>
+                  <TableCell align="right">Flight Home</TableCell>
+                  <TableCell align="right">Flight Number</TableCell>
+                  <TableCell align="right">Flight Comany</TableCell>
+                  <TableCell align="right">Destination</TableCell>
+                  <TableCell align="right">Flight Time</TableCell>
+                  <TableCell align="right">Flight Time Home</TableCell>
+                  <TableCell align="right">Seats</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {flights.map((flight) => (
+                  <TableRow>
+                    <TableCell align="right" component="th" scope="row">
+                      {flight.Response.dateTo}
+                    </TableCell>
+                    <TableCell align="right">{flight.Response.dateHome}</TableCell>
+                    <TableCell align="right">{flight.Response.flightNumber}</TableCell>
+                    <TableCell align="right">{flight.Response.flightComany}</TableCell>
+                    <TableCell align="right">{flight.Response.destination}</TableCell>
+                    <TableCell align="right">{flight.Response.flightTimeTo}</TableCell>
+                    <TableCell align="right">{flight.Response.flightTimeHome}</TableCell>
+                    <TableCell align="right">{flight.Response.seats}</TableCell>
+                    <Button align="right">Book Flight</Button>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+      </Modal>  
       </div>
     );
 }
