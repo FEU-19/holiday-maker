355ea45cb67e7df9ffd2750992439a4976a8c534
Anna Eklöv <annaeklov90@gmail.com>
2020-09-16 11:59:48 +0200

    styled buttons on header

--- client/src/components/common/Header/Header.js
+++ client/src/components/common/Header/Header.js
@@ -2,22 +2,25 @@ import React from "react";
 import styled from "styled-components";
 
 // Components
-import LoginModal from "../../modals/LoginModal"
+import LoginModal from "../../modals/LoginModal";
 
 // Styles
 const Container = styled.div`
   width: 100%;
   height: 100px;
-  background-color: #162C72;
+  background-color: #162c72;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  box-shadow: 0px 3px 5px -3px rgba(0,0,0,0.5);
+  box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.5);
+  position: relative;
 `;
 
 const ContainerBtns = styled.div`
-  padding-right: 10px;
+  position: absolute;
+  bottom: 5px;
+  right: 10px;
 `;
 
 const Logo = styled.img`
@@ -32,7 +35,7 @@ const Logo = styled.img`
 const Header = () => {
   return (
     <Container>
-      <Logo src={process.env.PUBLIC_URL + '/Holidaymaker-logo-BIG.png'} />
+      <Logo src={process.env.PUBLIC_URL + "/Holidaymaker-logo-BIG.png"} />
       <ContainerBtns>
         <LoginModal />
       </ContainerBtns>
