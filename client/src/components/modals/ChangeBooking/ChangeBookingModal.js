a2072fab83dcb8f950c5d17c7917d542f75672d7
Johanna <nijodotcom@gmail.com>
2020-09-16 08:42:15 +0200

    fixed bug in mockdata

--- client/src/components/modals/ChangeBooking/ChangeBookingModal.js
+++ client/src/components/modals/ChangeBooking/ChangeBookingModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Fragment } from "react";
-import axios from 'axios';
+import axios from "axios";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
@@ -14,58 +14,67 @@ import DatePicker from "../../Main/SearchContainer/DatePicker";
 
 import ChildrenAgeSelects from "../../Main/SearchContainer/ChildrenAgeSelects";
 
-import RenderRooms from './RenderRooms';
-
+import RenderRooms from "./RenderRooms";
 
 export default function ChangeBookingModal() {
   const [open, setOpen] = useState(true);
-  const [hotelId, setHotelId] = useState('5f5b7e5b36ac0355705b8087');
+  const [hotelId, setHotelId] = useState("5f5b7e5b36ac0355705b8087");
   const [hotel, setHotel] = useState(null);
   const [bookings, setBookings] = useState([
     {
-    "_id": { // boknings nummer
-        "$oid": "5f5889d8daa2064fd4eb8a42"
-    },
-    "userId": { // användare
-        "$oid": "5f5f64fb86170a41247bdf06"
-    },
-    "bookingNumber": "k7cSt78z9k9v9n261lrv5364e",
-    "rooms": [{
-        "_id": {
-            "$oid": "5f5b7e5b36ac0355705b8088"  
-        },
-        "price": 3672,
-        "option": "all-inclusive",
-        "roomNumber": "100"
-    }, {
-      "_id": {
-          "$oid": "5f5b7e5b36ac0355705b808d"
+      _id: {
+        // boknings nummer
+        $oid: "5f5889d8daa2064fd4eb8a42",
       },
-      "price": 3672,
-      "option": "all-inclusive",
-      "roomNumber": "100"
-  }],
-    "bookingDates": {
-        "start": "2020-06-01T11:46:29.258Z",
-        "end": "2020-06-15T11:47:09.886Z"
+      userId: {
+        // användare
+        $oid: "5f5f64fb86170a41247bdf06",
+      },
+      bookingNumber: "k7cSt78z9k9v9n261lrv5364e",
+      rooms: [
+        //   {
+        //     "_id": {
+        //         "$oid": "5f5b7e5b36ac0355705b8088"
+        //     },
+        //     "price": 0,
+        //     "option": "selfCatering",
+        //     "roomNumber": "100"
+        // },
+        {
+          _id: {
+            $oid: "5f5b7e5b36ac0355705b808c",
+          },
+          price: 531,
+          option: "halfBoard",
+          roomNumber: "103",
+        },
+        {
+          _id: {
+            $oid: "5f5b7e5b36ac0355705b8096",
+          },
+          price: 748,
+          option: "allInclusive",
+          roomNumber: "110",
+        },
+      ],
+      bookingDates: {
+        start: "2020-06-01T11:46:29.258Z",
+        end: "2020-06-15T11:47:09.886Z",
+      },
+      hotel: {
+        $oid: "5f5b7e5b36ac0355705b8087",
+      },
+      __v: 0,
     },
-    "hotel": {
-        "$oid": "5f5b7e5b36ac0355705b8087"
-    },
-    "__v": 0
-  }]); // ska få från parent
-  
-
-
+  ]); // ska få från parent
 
   useEffect(() => {
     axios
       .get(`http://localhost:8080/api/residences/${hotelId}`)
       .then((res) => {
         console.log(res);
-        
-        setHotel(res.data.data);
 
+        setHotel(res.data.data);
       })
       .catch((error) => {
         console.error(
@@ -79,7 +88,6 @@ export default function ChangeBookingModal() {
   // rendera ut vad rummet erbjuder
   // skicka in datan till RenderFoodOption
 
-
   // Få PUT att funka innan vi redirectar¨
   // Sedan Redirect to={{ pathname: "/checkout", state: { rooms: chosenRooms } }}
   // const [chosenRooms, setChosenRooms] = useState([]);
@@ -92,51 +100,48 @@ export default function ChangeBookingModal() {
     setOpen(false);
   };
   return (
-    
     <Dialog
       open={open}
       onClose={handleClose}
       aria-labelledby="form-dialog-title"
-    > 
-    
-    {!hotel ? (<p>Loading...</p>) :  
-    <div>
-      <DialogTitle id="form-dialog-title">{hotel.name}</DialogTitle>
-      <DialogContent>
-        <RenderRooms bookings={bookings} hotel={hotel}/>
-        {/* <DatePicker />
+    >
+      {!hotel ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <DialogTitle id="form-dialog-title">{hotel.name}</DialogTitle>
+          <DialogContent>
+            <RenderRooms bookings={bookings} hotel={hotel} />
+            {/* <DatePicker />
         <SelectAmountOfAdults />
         <SelectAmountOfChildren />
         <ChildrenAgeSelects /> */}
-        {/* Antal rum */}
-        {/* Extra bed */}
-        {/* flight */}
-        {/* Need price */}
+            {/* Antal rum */}
+            {/* Extra bed */}
+            {/* flight */}
+            {/* Need price */}
 
-        <RadioGroup
-          aria-label="price"
-          name="price"
-          // value={selected}
-          // onChange={handleChange}
-        > 
-        
-        </RadioGroup>
+            <RadioGroup
+              aria-label="price"
+              name="price"
+              // value={selected}
+              // onChange={handleChange}
+            />
 
-        <Button onClick={handleClose} color="primary">
-          Book flight
-        </Button>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel changes
-        </Button>
-        <Button onClick={handleClose} color="primary">
-          Save changes
-        </Button>
-      </DialogActions>
-      </div>}
-     
+            <Button onClick={handleClose} color="primary">
+              Book flight
+            </Button>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel changes
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Save changes
+            </Button>
+          </DialogActions>
+        </div>
+      )}
     </Dialog>
   );
-
 }
