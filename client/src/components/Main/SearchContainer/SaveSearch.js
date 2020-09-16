118e6ebb963ea3d40dbd80abd4721f45c3afa46b
MervatMimmi <mervat.hassan@gmail.com>
2020-09-16 20:14:28 +0200

    test

--- client/src/components/Main/SearchContainer/SaveSearch.js
+++ client/src/components/Main/SearchContainer/SaveSearch.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import Grid from "@material-ui/core/Grid";
+import React from "react";
 import { Button } from "@material-ui/core";
 
 const SaveSearch = ({savedSearchParams}) => {
-
   function saveSearch(){
     // Make post to server with savedSearchParams
     console.log(savedSearchParams);
   }
 
   return(
-      <Button
+      <Button 
+        size = "small"
         type="button"
         variant="contained"
         color="primary"
