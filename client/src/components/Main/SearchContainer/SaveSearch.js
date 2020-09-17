71b2e499b5ee7ecb74ae4e350a3d40e440f378fb
MervatMimmi <mervat.hassan@gmail.com>
2020-09-17 09:43:38 +0200

    merging

--- client/src/components/Main/SearchContainer/SaveSearch.js
+++ client/src/components/Main/SearchContainer/SaveSearch.js
@@ -1,12 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-import { Button, makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import {Bookmarks} from "@material-ui/icons";
-=======
-//import styled from "styled-components";
-//import Grid from "@material-ui/core/Grid";
-import { Button } from "@material-ui/core";
->>>>>>> f35a0cdfa2d671e402902964aab0066a1db1e435
 
 const useStyle = makeStyles(() => ({
   bookmarks: {
