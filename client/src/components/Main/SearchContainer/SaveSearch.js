b61f0c2e82226b7fe91d02d2902b8323a2becb25
Chau Kha Doan <kha.doanchau@gmail.com>
2020-09-18 17:29:09 +0200

    yayy

--- client/src/components/Main/SearchContainer/SaveSearch.js
+++ client/src/components/Main/SearchContainer/SaveSearch.js
@@ -1,1 +1,33 @@
 
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import { Bookmarks } from "@material-ui/icons";
+
+const useStyle = makeStyles(() => ({
+ bookmarks: {
+  marginTop: "5px",
+  "&:hover": {
+   cursor: "pointer",
+   opacity: "0.8",
+  },
+ },
+}));
+const SaveSearch = ({ savedSearchParams }) => {
+ const classes = useStyle();
+
+ function saveSearch() {
+  // Make post to server and save hotel ID in user bookmarkedHotels[]
+  console.log(savedSearchParams);
+ }
+
+ return (
+  <Bookmarks
+   className={classes.bookmarks}
+   color="primary"
+   placeholder="Save search"
+   onClick={saveSearch}
+  />
+ );
+};
+
+export default SaveSearch;
